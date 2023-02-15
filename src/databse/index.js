75b90425aacb89f8788bffedf65f61f7be540991
faizantourtech <114976850+faizantourtech@users.noreply.github.com>
2023-02-15 16:31:54 +0500

    change mongo url

--- src/databse/index.js
+++ src/databse/index.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-const databseUrl = process.env.MONGOOSE_URL
-mongoose.connect(databseUrl, {
+const databseUrl =
+  "mongodb+srv://mughees:mughees@cluster0.jgi2dhz.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(databseUrl, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("connected");
-})
-    .catch((e) => {
-        console.log("errr", e);
-    })
+  })
+  .catch((e) => {
+    console.log("errr", e);
+  });
